625ab6d3cdac68d00b87f59e7a59f040929daa27
Jeong-IK <jjy20315@gmail.com>
2022-09-06 19:43:32 +0900

    :adhesive_bandage: ESLint, Prettier 충돌 오류 해결(comma-dangle Error)

--- choose-encoding/.eslintrc.js
+++ choose-encoding/.eslintrc.js
@@ -6,14 +6,15 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
         "plugin:prettier/recommended",
-        "airbnb-base",
+        "airbnb-base"
         ],
     "overrides": [
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
+        "project" : "./tsconfig.json",
         "ecmaVersion": "latest",
         "sourceType": "module",
 
@@ -28,13 +29,16 @@ module.exports = {
             "semi": true,
             "useTabs": false,
             "tabWidth": 2,
-            "trailingComma": "all",
-            "printWidth": 80
-        }],
+            "trailingComma": "es5",
+            "printWidth": 80}],
+        "comma-dangle" : "off"
     },
     "settings":{
         "react":{
             "version":"detect"
-        }
+        },
+        "import/parsers": {
+            "@typescript-eslint/parser": [".ts", ".tsx"]
+          },
     }
 }
